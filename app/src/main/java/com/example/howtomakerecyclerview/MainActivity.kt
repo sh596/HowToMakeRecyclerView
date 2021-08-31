42badfb3a4010cfc5e0a9a627c6e517fc19b9424
samgashyeong <samgashyeong@naver.com>
2021-09-01 01:26:47 +0900

    수정

--- app/src/main/java/com/example/howtomakerecyclerview/MainActivity.kt
+++ app/src/main/java/com/example/howtomakerecyclerview/MainActivity.kt
@@ -6,42 +6,21 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.example.howtomakerecyclerview.databinding.ActivityMainBinding
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var recyclerView : RecyclerView
-    private lateinit var addBtn : Button
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        //view 설정
-        recyclerView = findViewById(R.id.recycler)
-        addBtn = findViewById(R.id.addBtn)
-
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         //array설정
         val list : ArrayList<Data> = ArrayList()
 
 
-        //adapter 설정
-        recyclerView.adapter = MainAdapter(list){ data, position ->
-            Toast.makeText(this, "아이템이 삭제됨", Toast.LENGTH_SHORT).show()
-            list.remove(data)
-            recyclerView.adapter!!.notifyItemRemoved(position)
-        }
-
-
-        //버튼을 눌렀을 때 아이템 추가
-        addBtn.setOnClickListener {
-            val randomInt = Random.nextInt(100).toString()
-            Log.d(TAG, "onCreate: random : $randomInt")
-            list.add(
-                Data("학생", R.drawable.ic_launcher_foreground, randomInt)
-            )
-            recyclerView.adapter!!.notifyItemInserted(list.size.minus(1))
-        }
     }
 
 }
--- app/src/main/java/com/example/howtomakerecyclerview/MainAdapter.kt
+++ app/src/main/java/com/example/howtomakerecyclerview/MainAdapter.kt
@@ -6,38 +6,8 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.howtomakerecyclerview.databinding.RowItemBinding
 
-class MainAdapter(val data : ArrayList<Data>, private val onClick: (data : Data, position : Int)-> Unit) : RecyclerView.Adapter<MainAdapter.ViewHolder>() {
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-        val itemText = itemView.findViewById<TextView>(R.id.itemText)
-        val rankText = itemView.findViewById<TextView>(R.id.rankText)
-        val itemImage = itemView.findViewById<ImageView>(R.id.imageView)
+class MainAdapter(){
 
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder { //ViewHolder 리턴함.
-
-        //일단 item에 넣을 xml을 설정
-        val v = LayoutInflater.from(parent.context).inflate(R.layout.row_item, parent, false)
-
-        //클릭 리스너를 위한 홀더를 미리 변수로 설정
-        val holder = ViewHolder(v)
-
-        //클릭했을 때 생성자 매개변수에 있는 함수 매개변수에 데이터를 전달
-        v.setOnClickListener {
-            onClick.invoke(data[holder.adapterPosition], holder.adapterPosition)
-        }
-        return holder
-    }
-
-    //리스트 데이터를 설정해주는 함수.
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.rankText.text = data[position].itemRankText
-        holder.itemImage.setImageResource(data[position].itemImageView)
-        holder.itemText.text = data[position].itemText
-    }
-
-
-    //아이템 크기 리턴.
-    override fun getItemCount(): Int = data.size
 }
